6c1f9145589eab25cf42ea711d709f7e613def0c
peter-amz <148516914+peter-amz@users.noreply.github.com>
2023-11-23 22:26:55 +0700

    Update Address.php

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -4,57 +4,57 @@ namespace LaravelShipStation\Models;
 class Address
 {
     /**
-     * @var string Name of person
+     * @var string|null Name of person
      */
     public $name;
 
     /**
-     * @var string Name of company
+     * @var string|null Name of company
      */
     public $company;
 
     /**
-     * @var string First line of address
+     * @var string|null First line of address
      */
     public $street1;
 
     /**
-     * @var string Second line of address
+     * @var string|null Second line of address
      */
     public $street2;
 
     /**
-     * @var string Third line of address
+     * @var string|null Third line of address
      */
     public $street3;
 
     /**
-     * @var string City
+     * @var string|null City
      */
     public $city;
 
     /**
-     * @var string State
+     * @var string|null State
      */
     public $state;
 
     /**
-     * @var string Postal Code
+     * @var string|null Postal Code
      */
     public $postalCode;
 
     /**
-     * @var string Country Code. The two-cahracter ISO country code is required.
+     * @var string|null Country Code. The two-cahracter ISO country code is required.
      */
     public $country;
 
     /**
-     * @var string Telephone number.
+     * @var string|null Telephone number.
      */
     public $phone;
 
     /**
-     * @var bool Specifies whether the given address is residential.
+     * @var bool|null Specifies whether the given address is residential.
      */
     public $residential;
 }
