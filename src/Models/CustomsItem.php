40b5471b38e0b93d41450729c33063d577d5bbcc
peter-amz <148516914+peter-amz@users.noreply.github.com>
2023-11-23 22:28:10 +0700

    Update CustomsItem.php

--- src/Models/CustomsItem.php
+++ src/Models/CustomsItem.php
@@ -4,27 +4,27 @@ namespace LaravelShipStation\Models;
 class CustomsItem
 {
     /**
-     * @var string A short description of the CustomItem
+     * @var string|null A short description of the CustomItem
      */
     public $description;
 
     /**
-     * @var int The quantity for this line item.
+     * @var int|null The quantity for this line item.
      */
     public $quantity;
 
     /**
-     * @var float The value (in USD) of the line item.
+     * @var float|null The value (in USD) of the line item.
      */
     public $value;
 
     /**
-     * @var string The Harmonized Commodity Code for this line item.
+     * @var string|null The Harmonized Commodity Code for this line item.
      */
     public $harmonizedTariffCode;
 
     /**
-     * @var string The 2-character ISO country code where the item originated.
+     * @var string|null The 2-character ISO country code where the item originated.
      */
     public $countryOfOrigin;
 }
