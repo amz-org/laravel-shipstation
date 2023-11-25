6a2a6e8204c8f3e26ff6d151d64982df732f8430
peter-amz <148516914+peter-amz@users.noreply.github.com>
2023-11-25 17:43:52 +0700

    Update OrderItem.php

--- src/Models/OrderItem.php
+++ src/Models/OrderItem.php
@@ -5,57 +5,57 @@ class OrderItem
 {
 
     /**
-     * @var string An identifier for the OrderItem in the originating system.
+     * @var string|null An identifier for the OrderItem in the originating system.
      */
     public $lineItemKey;
 
     /**
-     * @var string The SKU (stock keeping unit) identifier for the product associated with this line item.
+     * @var string|null The SKU (stock keeping unit) identifier for the product associated with this line item.
      */
     public $sku;
 
     /**
-     * @var string The name of the product associated with this line item.
+     * @var string|null The name of the product associated with this line item.
      */
     public $name;
 
     /**
-     * @var string The public URL to the product image.
+     * @var string|null The public URL to the product image.
      */
     public $imageUrl;
 
     /**
-     * @var array The weight of a single item.
+     * @var Weight|null The weight of a single item.
      */
     public $weight;
 
     /**
-     * @var int The quantity of the product ordered.
+     * @var int|null The quantity of the product ordered.
      */
     public $quantity;
 
     /**
-     * @var float The sell price of a single item specified by the order source.
+     * @var float|null The sell price of a single item specified by the order source.
      */
     public $unitPrice;
 
     /**
-     * @var float The tax price of a single item specified by the order source.
+     * @var float|null The tax price of a single item specified by the order source.
      */
     public $taxAmount;
 
     /**
-     * @var float The shipping amount or price of a single item specified by the order source.
+     * @var float|null The shipping amount or price of a single item specified by the order source.
      */
     public $shippingAmount;
 
     /**
-     * @var string The location of the product within the seller's warehouse (e.g. Aisle 3, Shelf A, Bin 5)
+     * @var string|null The location of the product within the seller's warehouse (e.g. Aisle 3, Shelf A, Bin 5)
      */
     public $warehouseLocation;
 
     /**
-     * @var array
+     * @var array|null
      */
     public $options;
 
@@ -65,19 +65,19 @@ class OrderItem
     public $productId;
 
     /**
-     * @var string The fulfillment SKU associated with this OrderItem if the fulfillment provider requires an
+     * @var string|null The fulfillment SKU associated with this OrderItem if the fulfillment provider requires an
      * identifier other than the SKU.
      */
     public $fulfillmentSku;
 
     /**
-     * @var bool Indicates that the OrderItem is a non-physical adjustment to the order
+     * @var bool|null Indicates that the OrderItem is a non-physical adjustment to the order
      * (e.g. a discount or promotional code).
      */
     public $adjustment = false;
 
     /**
-     * @var string UPC associated with this OrderItem.
+     * @var string|null UPC associated with this OrderItem.
      */
     public $upc;
 }
